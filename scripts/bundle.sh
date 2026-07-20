f17c70c12ef42c8ac51baee2af293ab98ae5c98b
Color <tao.zhou@longbridge-inc.com>
2026-07-20 13:15:00 +0800

    bump to 0.8.1

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -54,8 +54,8 @@ cat > "${OUT}/Contents/Info.plist" <<PLIST
     <key>CFBundleExecutable</key><string>${APP_NAME}</string>
     <key>CFBundleIconFile</key><string>AppIcon</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>0.8.0</string>
-    <key>CFBundleVersion</key><string>8</string>
+    <key>CFBundleShortVersionString</key><string>0.8.1</string>
+    <key>CFBundleVersion</key><string>9</string>
     <key>LSMinimumSystemVersion</key><string>14.0</string>
     <key>NSPrincipalClass</key><string>NSApplication</string>
     <key>NSHighResolutionCapable</key><true/>
